e2f8bcb09099225b2ab8d1d553bb453d90c6998e
cll123456 <2642137848@qq.com>
2022-03-31 11:03:51 +0800

    chore:  格式化verifyCommit的代码格式

--- scripts/verifyCommit.ts
+++ scripts/verifyCommit.ts
@@ -9,7 +9,32 @@ const msg = readFileSync(msgPath, 'utf-8').trim()
 
 const releaseRE = /^v\d/
 const commitRE =
-  /^(revert: )?(feat|fix|docs|dx|refactor|perf|test|workflow|build|ci|chore|types|wip|release|deps)(\(.+\))?: .{1,50}/
+  /^(revert: )?(feat|bug|fix|ui|docs|style|perf|release|deploy|refactor|test|chore|revert|merge|build)(\(.+\))?: .{1,50}/
+const msgObj: any = {
+  feat: '新功能(feature)',
+  bug: '此项特别针对bug号,用于向测试反馈bug列表的bug修改情况',
+  ui: '更新 ui',
+  fix: '修复bug(fix)',
+  docs: '文档(documentation)',
+  style: '格式,不影响代码运行的变动(style)',
+  perf: '性能性能优化(performance)',
+  release: '发布(release)',
+  deploy: '部署(deploy)',
+  refactor: '重构,即不是新增功能,也不是修改bug的代码变动(refactor)',
+  test: '单元测试(test)',
+  chore: '构建过程或辅助工具的变动(chore)',
+  revert: '回滚(revert)',
+  merge: '合并分支(merge)',
+  build: '构建(build)',
+}
+
+let msgStr = ''
+for (const key in msgObj) {
+  if (Object.prototype.hasOwnProperty.call(msgObj, key)) {
+    const element = msgObj[key]
+    msgStr += `    ${key}: ` + element + '\n'
+  }
+}
 
 if (!releaseRE.test(msg) && !commitRE.test(msg)) {
   console.log()
@@ -20,9 +45,7 @@ if (!releaseRE.test(msg) && !commitRE.test(msg)) {
       colors.red(
         `  Proper commit message format is required for automated changelog generation. Examples:\n\n`,
       ) +
-      `    ${colors.green(`feat: add 'comments' option`)}\n` +
-      `    ${colors.green(`fix: handle events on blur (close #28)`)}\n\n` +
-      colors.red(`  See .github/commit-convention.md for more details.\n`),
+      `${colors.green(`${msgStr}`)}\n\n`,
   )
   process.exit(1)
 }
