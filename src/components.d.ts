dbbce201e8eeec16d2c21621d6f9cef27fd1dc07
Cll12345 <2642137848@qq.com>
2022-09-24 18:09:31 +0800

    chore: 🐳 更新依赖

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,18 +7,13 @@ declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     Header: typeof import('./components/Header/index.vue')['default']
     Navbar: typeof import('./components/Navbar/index.vue')['default']
-    Ripple: typeof import('@varlet/ui')['_RippleComponent']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     VarAppBar: typeof import('@varlet/ui')['_AppBarComponent']
     VarBadge: typeof import('@varlet/ui')['_BadgeComponent']
     VarButton: typeof import('@varlet/ui')['_ButtonComponent']
     VarCard: typeof import('@varlet/ui')['_CardComponent']
-    VarCell: typeof import('@varlet/ui')['_CellComponent']
-    VarDivider: typeof import('@varlet/ui')['_DividerComponent']
     VarIcon: typeof import('@varlet/ui')['_IconComponent']
-    VarList: typeof import('@varlet/ui')['_ListComponent']
-    VarLoading: typeof import('@varlet/ui')['_LoadingComponent']
     VirtualList: typeof import('./components/VirtualList/index.vue')['default']
   }
 }
